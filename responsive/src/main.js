de352ee8a183edded5244ce1e9a551d03e73f1ea
Ignácz Dominik Bence <dominikignacz90@gmail.com>
2026-04-26 16:31:07 +0200

    vizsga alap

---
+++ responsive/src/main.js
@@ -0,0 +1,14 @@
+/**
+ * Amennyiben a feladatot Bootstrap segítségével szeretné megoldani, vegye ki az alábbi kommentet:
+ */
+
+// import "@assets/external/bootstrap.min.css";
+
+/**
+ * Amennyiben a feladatot TailwindCSS segítségével szeretné megoldani, vegye ki az alábbi kommentet:
+ */
+
+// import "@assets/external/tailwind.css";
+
+import "@assets/app.css";
+import "@assets/style.css";
--- start.sh
+++ start.sh
@@ -16,6 +16,7 @@ fi
 
 docker run --rm  -v "$(pwd)/frontend:/app" -v "shared_pnpm:/shared_pnpm/" --entrypoint pnpm  idomi27/vue:26 install --dangerously-allow-all-builds
 
+docker run --rm  -v "$(pwd)/responsive:/app" -v "shared_pnpm:/shared_pnpm/" --entrypoint pnpm  idomi27/vue:26 install --dangerously-allow-all-builds
 
 docker compose up -d
 
